2c416e21289aa536fde5fd5734c0838e639aab40
shivam1841 <shivam1841@gmail.com>
2016-03-18 22:37:28 -0400

    Second Commit

---
+++ Assets/HeroController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroController : MonoBehaviour
+{
+    //private variables declaration
+    private Animation _animation;
+    private float _movement;
+    private float _charge;
+
+    // Use this for initialization
+    void Start()
+    {
+        this._animation = gameObject.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
